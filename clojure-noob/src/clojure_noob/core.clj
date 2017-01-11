f10bd9504e4de6b33c22952d2db98858bb71ee17
Purush Swaminathan <purukaushik@asu.edu>
2017-01-11 00:37:43 -0700

    Emacs chapter--barfing

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -5,3 +5,7 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Dei, mokka. Idhaan clojure!"))
+(println "Manidha! oh Manidha! oh Manidha! Ooh Manidha!")nil
+(defn train
+  []
+  (println "Choo!Choo! Chee!Chee! Veidi phone-ah! Veidi!"))
