e8c840d147150da6560c7a1286bd104cfebf3ecd
Purush Swaminathan <purukaushik@asu.edu>
2017-01-11 23:33:35 -0700

    If, do, when. do-things

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -9,3 +9,8 @@
 (defn train
   []
   (println "Choo!Choo! Chee!Chee! Veidi phone-ah! Veidi!"))
+(+ 1 (* 2 3 4))
+(if true "By Zeus's Armor" "This wont happen!")
+(if (< 0 1) (if (< -1 0) "True") "False")
+(if true (do (println "String Output") "Print") "Wont be printed")
+(when true (println (str "Appending " "Strings")))
