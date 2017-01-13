237fa6222c93f8d06b2080d4137480aca1b1d24f
Purush Swaminathan <purukaushik@asu.edu>
2017-01-13 10:43:20 -0700

    Lists, vectors, maps, sets, till functions
    Till functions

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,3 +1,5 @@
+;; Run all examples here with C-x C-e after last S-exp on emacs with clojure mode and cider
+
 (ns clojure-noob.core
   (:gen-class))
 
@@ -48,3 +50,70 @@ failed-protagonist-names
 
 ;; def is for values, defn is for functions
 
+;; Clojure maps -> sorted and hash maps
+
+;; empty map
+{}
+;; Keyword keys
+{:f 1 :g 2}
+;; String keys
+{"1" 1}
+;; vv Throws error!! vv
+;; {1 2 3 5}
+
+;; hash maps
+(def my-map (hash-map :a 1 :b 2))
+
+;; checking maps for equality
+(def their-map {:a 1 :b 2})
+(= their-map my-map) ;;returns false -> maybe object model for equality like java???
+
+;; hash map get()
+my-map
+(get my-map :a) ;; => 1
+
+;; default values when key is not in map
+(get my-map :c) ;; => nil
+(get my-map :c "Nothing dude")
+
+;; nested maps
+(def nested (hash-map :a (hash-map :b 1)))
+;; get inside nested map
+(get-in my-map [:b]) ;; => 2
+
+;; map get is actually you trying to query a map like it is a function
+({:name "The human teapot"} :name)
+;; this works too for some odd reason
+(:name {:name "The human teapot"})
+
+;; :name is a keyword in clojure
+
+;; Vectors
+;; obviously hybrid
+[3 45 -1 "hello"]
+;; getting stuff by index
+((get ["a" {:name "Pugsley Bottomwinter"} "c" ] 1) :name)
+;; vector function creates vectors obviously
+(vector "creepy" "full" "moon")
+;; let's see if we can concatenate a list of strings
+(str ["creepy" "full" "moon"])
+;; Consing vectors
+(conj [1 2 3] 4)
+
+;; Lists are not vectors !!
+'(1 2 3 4) ;; the lisp way of course
+;; get by index
+(nth '(:a :b :c) 0)
+(conj '(0 2 1) -1) ;; adds to head of list
+
+;; Sets -> again hashSets, sortedSets
+;; again hybrid
+
+;; Hash set ab initio
+#{"kurt vonnegut" :p :e :i9 00} ;; duplicates cause problems
+
+;; functionalized
+(hash-set 1 2 2 3 1 2) ;; duplicates dont bother us this way
+;; sets would need contains
+(contains? #{:a :b} :a)
+(contains? #{:a :b} :c)
